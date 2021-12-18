245b8e81c59fc957a8bb24a0bc91205ec21208e5
BuildTools <unconfigured@null.spigotmc.org>
2021-12-17 21:21:25 -0800

    test

--- components/NHLStats.js
+++ components/NHLStats.js
@@ -4,12 +4,13 @@ import { useEffect, useState } from 'react'
 import { supabase } from "../utils/supabaseClient.js"
 
 export default function NHLStats({ user }) {
-
     var username = user.email.substr(0, user.email.indexOf('@'));
     /* Everything went as expected, show full app */
     return (
         <div className="bg-white min-w-screen center">
             <p1>Hello, {username}!</p1>
+            <iframe src="https://public.tableau.com/views/GameScore_16398022617310/Sheet1?:language=en-US&:retry=yes&:display_count=n&:origin=viz_share_link&:showVizHome=no" width="1900
+            vw" height="850vh"></iframe>
         </div>
     )
 }
